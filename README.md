1188f78fb3688c7f9c68d675477ffdbd4ca08082
Jonathan Chacón <jonathan.chacon@cabify.com>
2021-10-30 07:00:04 +0200

    Initial readme.md file and the SystemSoundManager class

--- README.md
+++ README.md
@@ -1,3 +1,40 @@
 # SoundManager
 
-A description of this package.
+A simple framework to load and play sounds in your app.
+
+## Supported platforms
+
+This framework is compatible with iOS, ipadOS and TvOS.
+
+## Usage
+
+* Import the module in your source file
+* Load a sound once
+* Play the sound as many times as you want without loading the file again
+
+You can use the alias **Sounds** to manage the library.
+
+### Sample code
+
+`import SoundManager
+
+// Load the sound file once
+Sounds.loadSound("My sound", fileName: "mySampleSound.wav")
+// And play it
+Sounds.playSound("My sound")
+// vibrate your device
+Sounds.vibrate()`
+
+## Author
+
+This package was developed by Jonathan Chacón .
+
+Please, if you have any question or suggestion you can contact me at [Tyflos Accessible Software](https://www.tyflosaccessiblesoftware.com) web site.
+
+## Contributing
+
+Pull requests are welcome. Feel free to create pull requests for any kind of improvements, bug fixes or enhancements. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+
+This software was published under the [MIT license](https://choosealicense.com/licenses/mit/)
--- Sources/SoundManager/SoundManager.swift
+++ Sources/SoundManager/SoundManager.swift
@@ -1,6 +1,80 @@
-public struct SoundManager {
-    public private(set) var text = "Hello, World!"
+import Foundation
+import AudioToolbox
+import AVFoundation
 
-    public init() {
+public let Sounds: SystemSoundEngine = SystemSoundEngine.shared
+
+final public class SystemSoundEngine {
+    static public let shared = SystemSoundEngine()
+    private var sounds = [String : SystemSoundID]()
+    private var soundMuted : Bool = false
+    private var vibrationMuted : Bool = false
+
+    public func muteSound(_ value : Bool) {
+        soundMuted = value
+    }
+    
+    public func muteVibration(_ value : Bool) {
+        vibrationMuted = value
+    }
+    
+    public func vibrate() {
+        if !vibrationMuted {
+            AudioServicesPlayAlertSound ( SystemSoundID ( kSystemSoundID_Vibrate ))
+        }
+    }
+    
+    public func loadSound(_ name: String , fileName : String, fileExtension : String = "") {
+        if let soundUrl = Bundle.main.url(forResource: fileName, withExtension: fileExtension) {
+            var soundId: SystemSoundID = 0
+            AudioServicesCreateSystemSoundID(soundUrl as CFURL, &soundId)
+            AudioServicesAddSystemSoundCompletion(soundId, nil, nil, { (soundId, clientData) -> Void in
+                // AudioServicesDisposeSystemSoundID(soundId)
+            }, nil)
+            sounds[name] = soundId
+        } else {
+            print("SoundEngine error:\nError loading sound " + name + "\n File does not exist:" + fileName + " extension:" + fileExtension)
+        }
+    }
+    
+    public func loadAllSounds(_ withExtension: String) {
+        let documentDir = Bundle.main.bundleURL
+        do {
+            let directoryContents = try FileManager.default.contentsOfDirectory(at: documentDir, includingPropertiesForKeys: nil)
+            let audioFiles = directoryContents.filter{ $0.pathExtension == withExtension }
+            let audioFileNames = audioFiles.map{ $0.deletingPathExtension().lastPathComponent }
+            for fileName in audioFileNames {
+                self.loadSound(fileName, fileName: fileName, fileExtension: withExtension)
+            }
+        } catch {
+            print("SoundEngine error:\nError unloading all sound files: \(error)")
+        }
+    }
+    
+    public func unloadSound(_ name : String) {
+        let soundId: SystemSoundID? = sounds[name]
+        if soundId != nil {
+            AudioServicesDisposeSystemSoundID(soundId!)
+        } else {
+            print("SoundEngine error:\nError unloading sound " + name)
+        }
+    }
+    
+    public func unloadAllSounds() {
+        for name in sounds.keys {
+            self.unloadSound(name)
+        }
+    }
+    
+    public func playSound(_ name : String) {
+        if soundMuted {
+            return
+        }
+        let soundId: SystemSoundID? = sounds[name]
+        if soundId != nil {
+            AudioServicesPlaySystemSound(soundId!)
+        } else {
+            print("SoundEngine error:\nSound not available " + name)
+        }
     }
 }
