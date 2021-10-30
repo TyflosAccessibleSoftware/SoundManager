94745fbdb1928bb2c57d011761d754bed04aeae6
Jonathan Chacón <jonathan.chacon@cabify.com>
2021-10-30 06:33:52 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SoundManager
+
+A description of this package.
---
+++ Tests/SoundManagerTests/SoundManagerTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SoundManager
+
+final class SoundManagerTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SoundManager().text, "Hello, World!")
+    }
+}
